53f28ea12f155eb568530bf38be9d865333afcca
rem <ahmedelsherif.osama@gmail.com>
2025-12-01 14:47:54 +0200

    fix(ProductRepository): add @Repository to ensure Spring tracks this and handles it properly

--- src/main/java/com/ahmed/ecommerce/ecommerce/product/ProductRepository.java
+++ src/main/java/com/ahmed/ecommerce/ecommerce/product/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.ahmed.ecommerce.ecommerce.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
