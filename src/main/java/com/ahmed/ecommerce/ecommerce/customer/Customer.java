4cfc89f4a257384dd37c8f6ee5b7bb3988ed7c6a
rem <ahmedelsherif.osama@gmail.com>
2025-12-04 13:33:34 +0200

    feat(Customer): add Customer Entity

---
+++ src/main/java/com/ahmed/ecommerce/ecommerce/customer/Customer.java
@@ -0,0 +1,33 @@
+package com.ahmed.ecommerce.ecommerce.customer;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Address> addresses;
+
+    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL)
+    private StoreWallet wallet;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<PaymentMethod> paymentMethods;
+
+
+}
