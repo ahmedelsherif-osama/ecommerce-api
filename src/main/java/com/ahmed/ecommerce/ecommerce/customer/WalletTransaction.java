b15361f44e37ff8c3a4643b9601c7a28e9e949e3
rem <ahmedelsherif.osama@gmail.com>
2025-12-04 13:42:51 +0200

    feat(WalletTransaction): add WalletTransaction Entity to separate Wallet from Transactions and enable Transaction History Tracking

---
+++ src/main/java/com/ahmed/ecommerce/ecommerce/customer/WalletTransaction.java
@@ -0,0 +1,30 @@
+package com.ahmed.ecommerce.ecommerce.customer;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+public class WalletTransaction {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    @ManyToOne
+    private StoreWallet wallet;
+
+    private BigDecimal amount;
+    private TransactionType type;
+    private String description;
+
+    private LocalDateTime createdAt;
+
+}
