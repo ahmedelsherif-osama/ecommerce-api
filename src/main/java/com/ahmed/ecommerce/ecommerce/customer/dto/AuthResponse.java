c76739c2b961dbc64715bc28d0fa01a75286c3a8
rem <ahmedelsherif.osama@gmail.com>
2025-12-04 14:25:57 +0200

    feat(AuthResponse): add AuthResponse Dto for holding token upon authentication success

---
+++ src/main/java/com/ahmed/ecommerce/ecommerce/customer/dto/AuthResponse.java
@@ -0,0 +1,5 @@
+package com.ahmed.ecommerce.ecommerce.customer.dto;
+
+public record AuthResponse(
+        String token
+) {}
---
+++ src/main/java/com/ahmed/ecommerce/ecommerce/customer/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package com.ahmed.ecommerce.ecommerce.customer.dto;
+
+public record LoginRequest(
+        String email,
+        String password
+) {}
+
