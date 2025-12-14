a30e0393d95a2e260bcd06d8ee9d6c691ee95c1d
rem <ahmedelsherif.osama@gmail.com>
2025-12-14 02:28:45 +0200

    chore(UserNotFoundException):update message to User not found

--- src/main/java/com/ahmed/ecommerce/ecommerce/exceptions/notfound/UserNotFoundException.java
+++ src/main/java/com/ahmed/ecommerce/ecommerce/exceptions/notfound/UserNotFoundException.java
@@ -6,9 +6,9 @@ import com.ahmed.ecommerce.ecommerce.exceptions.ResourceNotFoundException;
 
 import java.util.UUID;
 
-public class CustomerNotFoundException extends ResourceNotFoundException {
-    public CustomerNotFoundException(UUID id) {
-        super("Customer with id " + id + " not found");
+public class UserNotFoundException extends ResourceNotFoundException {
+    public UserNotFoundException(UUID id) {
+        super("User with id " + id + " not found");
     }
 }
 
