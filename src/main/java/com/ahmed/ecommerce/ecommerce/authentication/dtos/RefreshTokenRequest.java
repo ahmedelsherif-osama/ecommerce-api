b5a09b6dff9244e226af1bcb59f0226fcd252418
rem <ahmedelsherif.osama@gmail.com>
2025-12-14 02:38:49 +0200

    feat(OAuthTokenService): create dummy placeholder Oauth Token Service

---
+++ src/main/java/com/ahmed/ecommerce/ecommerce/authentication/dtos/RefreshTokenRequest.java
@@ -0,0 +1,13 @@
+package com.ahmed.ecommerce.ecommerce.authentication.dtos;
+
+public class RefreshTokenRequest {
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
