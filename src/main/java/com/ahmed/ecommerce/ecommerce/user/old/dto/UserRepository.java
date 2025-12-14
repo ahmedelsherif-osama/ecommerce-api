53c6dc987f22ac64b25898d46d7c9944d54a35fc
rem <ahmedelsherif.osama@gmail.com>
2025-12-14 02:29:32 +0200

    fix(UserRepository): add findByEmail custom function

--- src/main/java/com/ahmed/ecommerce/ecommerce/user/old/dto/UserRepository.java
+++ src/main/java/com/ahmed/ecommerce/ecommerce/user/old/dto/UserRepository.java
@@ -1,9 +1,11 @@
-package com.ahmed.ecommerce.ecommerce.user;
+package com.ahmed.ecommerce.ecommerce.user.old.dto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
