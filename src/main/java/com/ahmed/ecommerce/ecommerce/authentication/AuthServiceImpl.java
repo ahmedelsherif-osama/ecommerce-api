cc0c3ac1eb2909ceaab761cebc84ed4ac203fade
rem <ahmedelsherif.osama@gmail.com>
2025-12-14 02:34:22 +0200

    feat(AuthService): implement authservice

---
+++ src/main/java/com/ahmed/ecommerce/ecommerce/authentication/AuthServiceImpl.java
@@ -0,0 +1,82 @@
+package com.ahmed.ecommerce.ecommerce.authentication;
+
+import com.ahmed.ecommerce.ecommerce.user.User;
+import com.ahmed.ecommerce.ecommerce.user.UserService;
+import com.ahmed.ecommerce.ecommerce.user.dto.AuthResponse;
+import com.ahmed.ecommerce.ecommerce.user.dto.LoginRequest;
+import com.ahmed.ecommerce.ecommerce.user.dto.UserRegistrationRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final UserService userService;
+    private final TokenService tokenService;
+
+    public AuthServiceImpl(UserService userService, TokenService tokenService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    public AuthResponse register(UserRegistrationRequest request) {
+        User user = userService.create(request);
+        String token = tokenService.createToken(user)
+                .orElseThrow(() -> new RuntimeException("Token creation not supported for this strategy"));
+        return new AuthResponse(token);
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        User user = userService.findByEmailAndPassword(request.email(), request.password());
+        String token = tokenService.createToken(user)
+                .orElseThrow(() -> new RuntimeException("Token creation not supported for this strategy"));
+        return new AuthResponse(token);
+    }
+
+    @Override
+    public AuthResponse refresh(String refreshToken) {
+        if (!tokenService.validate(refreshToken)) {
+            throw new RuntimeException("Invalid token");
+        }
+        UUID userId = tokenService.extractUserId(refreshToken)
+                .orElseThrow(() -> new RuntimeException("Cannot extract user ID from token"));
+        User user = userService.findById(userId);
+
+        String token = tokenService.createToken(user)
+                .orElseThrow(() -> new RuntimeException("Token creation not supported for this strategy"));
+        return new AuthResponse(token);
+    }
+
+    /* For future enablement of AuthType selection from user/frontend's side
+    public AuthResponse login(LoginRequest request) {
+    TokenService tokenService;
+    User user;
+
+    switch(request.getAuthMethod()) {
+        case "EMAIL":
+            user = userService.findByEmailAndPassword(request.email(), request.password());
+            tokenService = tokenServices.get("jwtTokenService");
+            break;
+        case "GOOGLE":
+            user = oauthService.verifyGoogleToken(request.getOauthToken());
+            tokenService = tokenServices.get("oauthTokenService");
+            break;
+        case "GITHUB":
+            user = oauthService.verifyGithubToken(request.getOauthToken());
+            tokenService = tokenServices.get("oauthTokenService");
+            break;
+        default:
+            throw new RuntimeException("Unknown auth method");
+    }
+
+    String token = tokenService.createToken(user)
+            .orElseThrow(() -> new RuntimeException("Token creation not supported"));
+
+    return new AuthResponse(token);
+}
+
+     */
+}
